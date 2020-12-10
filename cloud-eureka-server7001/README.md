2681f7e6a9892507f50087dc7ccb8b9b08a81928
ge yangchen <geyangchengyc@163.com>
2020-12-10 18:17:15 +0800

    cloud-eureka-server7001

---
+++ cloud-eureka-server7001/README.md
@@ -0,0 +1,8 @@
+eureka-server 服务注册中心
+-   依赖：
+```xml
+<dependency>
+    <groupId>org.springframework.cloud</groupId>
+    <artifactId>spring-cloud-starter-netflix-eureka-server</artifactId>
+</dependency>
+```
---
+++ cloud-eureka-server7001/src/main/java/com/gyc/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.gyc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by GYC
+ * 2020/12/10 17:42
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+
+}
