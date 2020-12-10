98796879fa82da86f95094943dd895fc0c20b0af
ge yangchen <geyangchengyc@163.com>
2020-12-10 20:03:59 +0800

    单机Eureka搭建成功

--- cloud-consumer-order80/src/main/java/com/gyc/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/gyc/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.gyc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by GYC
  * 2020/12/8 17:36
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-provider-payment8001/src/main/java/com/gyc/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/gyc/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.gyc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by GYC
  * 2020/12/8 14:03
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
