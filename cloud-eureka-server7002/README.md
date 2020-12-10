e04fd41284fdfbb1093a3aa18d73c3fff3fad0ef
ge yangchen <geyangchengyc@163.com>
2020-12-11 00:06:07 +0800

    Eureka集群搭建成功

---
+++ cloud-eureka-server7002/README.md
@@ -0,0 +1,3 @@
+eureka-server 服务注册中心集群
+-   7001 eureka7001.com
+-   7002 eureka7002.com
---
+++ cloud-eureka-server7002/src/main/java/com/gyc/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.gyc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by GYC
+ * 2020/12/10 20:19
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+
+}
