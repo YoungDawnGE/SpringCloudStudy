cbe806bbb8b6d4e2d8eba047afbbbc79edf09e3d
ge yangchen <geyangchengyc@163.com>
2021-01-10 10:07:52 +0800

    HystrixDashboard (暂时没配置成功Unable to connect to Command Metric Stream.)

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/gyc/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.gyc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by GYC
+ * 2021/1/9 15:10
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
