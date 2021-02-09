5aacb7152d06dc5c938a73cdf3022c1d08ea83aa
ge yangchen <geyangchengyc@163.com>
2021-02-09 11:04:48 +0800

    cloud-stream-rabbitmq-consumer8002 消息驱动之消费者

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/gyc/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.gyc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by GYC
+ * 2021/2/9 10:37
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/gyc/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,26 @@
+package com.gyc.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by GYC
+ * 2021/2/9 10:43
+ */
+@RestController
+//@RequestMapping("/stream")
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    //监听输入源
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        System.out.println("消费者2号(port=" + serverPort + ")接收数据：" + message.getPayload());
+    }
+}
